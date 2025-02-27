d3d125ac94c415fd9fe0555bb1a695934a963a16
eepasion <elias.pasion@pragma.com.co>
2025-02-27 15:10:33 -0500

    feat: add security

--- src/main/java/com/pragma/bootcamp/infrastructure/config/RsocketConfiguration.java
+++ src/main/java/com/pragma/bootcamp/infrastructure/config/RsocketConfiguration.java
@@ -1,12 +1,14 @@
 package com.pragma.bootcamp.infrastructure.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.util.MimeType;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
@@ -15,8 +17,9 @@ import java.time.Duration;
 public class RsocketConfiguration {
 
     @Bean
-    public RSocketRequester rSocketRequester(RSocketRequester.Builder builder) {
+    public RSocketRequester rSocketRequester(RSocketRequester.Builder builder,@Value("${security.api-key}") String apiKey) {
         return builder
+                .setupMetadata(apiKey, MimeType.valueOf("application/text"))
                 .rsocketConnector(connector ->
                         connector.reconnect(Retry.fixedDelay(2, Duration.ofSeconds(2)))
                                 .dataMimeType("application/json"))
